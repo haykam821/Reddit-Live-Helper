98c33dae0e31d8894de39a42d05b8f988b7f465d
haykam821 <haykam821@users.noreply.github.com>
2018-03-30 07:15:50 -0400

    Update

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const config = require("./config.json");
 const snoowrap = require("snoowrap");
 
+const yargs = require("yargs");
+
 const reddit = new snoowrap(config.auth);
 
 const thread = reddit.getLivethread("10otrx267owkb");
 thread.stream.on("update", data => {
-    console.log(data);
+    const msg = data.body.replace(">", "");
+    if (!data.body.startsWith(">")) return;
+
+    yargs.command("test", "A simple test command.", {
+        "addDelay": {
+            description: "An addition delay before returning the message.",
+            default: 0,
+            type: "number",
+        }
+    }, args => {
+        setTimeout(function() {
+            thread.addUpdate("The test is complete!");
+        }, args.addDelay);
+    });
+    yargs.parse(msg);
 });
