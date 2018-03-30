82de09af52bbb4a699ad42a841bb3e87b8e68b94
haykam821 <haykam821@users.noreply.github.com>
2018-03-30 16:53:51 -0400

    THINGS WORK

--- index.js
+++ index.js
@@ -4,22 +4,56 @@ const snoowrap = require("snoowrap");
 const yargs = require("yargs");
 
 const reddit = new snoowrap(config.auth);
+const myself = reddit.getMe();
+
+const mathjs = require("mathjs");
 
 const thread = reddit.getLivethread("10otrx267owkb");
+function send(msg) {
+    thread.addUpdate(msg);
+}
+
+function makeBlock(msg) {
+    const parts = msg.split("\n");
+    return parts.map(item => {
+        return "    " + item;
+    }).join("\n");
+}
+
+let lastParsing = null;
+
 thread.stream.on("update", data => {
-    const msg = data.body.replace(">", "");
-    if (!data.body.startsWith(">")) return;
+    const msg = data.body.replace(config.prefix, "");
+    if (!data.body.startsWith(config.prefix) || data.author.name === myself.name) return;
 
     yargs.command("test", "A simple test command.", {
-        "addDelay": {
+        "delay": {
             description: "An addition delay before returning the message.",
             default: 0,
             type: "number",
         }
     }, args => {
         setTimeout(function() {
-            thread.addUpdate("The test is complete!");
-        }, args.addDelay);
+            send("The test is complete!");
+        }, args.delay);
     });
-    yargs.parse(msg);
+    yargs.command("whoami", "Find out who you are.", {}, args => {
+        send(`You are u/${data.author.name}.`);
+    });
+    yargs.command("debug", "Outputs the debug for the last parsed command.", {}, args => {
+        if (lastParsing) {
+            send(`Here is the debug for the last parsed command:\n\n${makeBlock(lastParsing)}`);
+        } else {
+            send(`I do not remember any previous commands. Sorry!`);
+        }
+    });
+    //yargs.command("sum", "Gets the sum of the provided numbers.", {}, )
+
+    yargs.help(false);
+    yargs.command("help", "Commands and stuff!", {}, () => {
+        const helpText = yargs.getUsageInstance().help();
+        send(makeBlock(helpText));
+    })
+
+    lastParsing = JSON.stringify(yargs.parse(msg), null, 4);
 });
