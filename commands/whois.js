4965ab0464825e3ad8ab08c78ee1c30bca5c344c
haykam821 <haykam821@users.noreply.github.com>
2018-04-06 23:31:23 -0400

    Remove extraneous code used for testing in whois command

--- commands/whois.js
+++ commands/whois.js
@@ -3,7 +3,6 @@ exports.describe = "Get information on a Reddit user.";
 exports.builder = {};
 exports.handler = async args => {
 	const user = await args.reddit.getUser(args.username).fetch();
-	console.log(user);
 	args.respond(`**u/${user.name}** ${user.is_mod ? "🛡" : ""}${user.is_gold ? "⭐" : ""}${user.has_verified_email ? "📧" : ""} has ${user.comment_karma + user.link_karma} total karma:
 	
 	* ${user.comment_karma} comment karma
