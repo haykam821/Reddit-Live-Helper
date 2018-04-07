dd44ab4abdaecfec3a22a28d13971c2ecb547d06
haykam821 <haykam821@users.noreply.github.com>
2018-04-06 23:14:50 -0400

    Whois command

---
+++ commands/whois.js
@@ -0,0 +1,14 @@
+exports.command = "whois <username>";
+exports.describe = "Get information on a Reddit user.";
+exports.builder = {};
+exports.handler = async args => {
+	const user = await args.reddit.getUser(args.username).fetch();
+	console.log(user);
+	args.respond(`**u/${user.name}** ${user.is_mod ? "🛡" : ""}${user.is_gold ? "⭐" : ""}${user.has_verified_email ? "📧" : ""} has ${user.comment_karma + user.link_karma} total karma:
+	
+	* ${user.comment_karma} comment karma
+	* ${user.link_karma} post karma
+	
+	This account was created on ${new Date(user.created_utc * 1000)}.
+	`);
+};
